2fe62d166a252526f53d11a868691e7c21c2ef16
starP-W <swteairP@outlook.com>
2022-04-18 23:28:29 +0800

    设置修改 添加部分逻辑

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 安装好Docker及Docker-compose，预留80、443、11514端口，然后
 ```
 mkdir caddy-grpc && cd caddy-grpc
-wget https://raw.githubusercontent.com/starP-W/caddy-grpc-docker/main/install.sh -O ./install.sh && bash install.sh
+curl -O https://raw.githubusercontent.com/starP-W/caddy-grpc-docker/main/install.sh && chmod +x install.sh && bash install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-stty erase ^H
+#stty erase ^H
 Precheck() {
     DOCKER_V=$(docker -v | grep -i "version")
     DC_V=$(docker-compose -v | grep -i "version")
@@ -36,6 +36,7 @@ ChangeSettings() {
 }
 
 SetCF() {
+    
     read -p "Please Input Your CloudFlare Mailbox: " MAILBOX
     read -p "Please Input Your CloudFlare API_Key: " APIKEY
     sed -i -E "s|CF_Email=.+|CF_Email=$MAILBOX|g" ./docker-compose.yml
@@ -48,19 +49,26 @@ SetCaddy() {
     read -p "Please Input Your Domain(eg. aa.org ): " FQDN
     sed -i -E "1s|^.+ \{|https://$FQDN \{|g" ./Caddyfile
     # servicename跟随UUID一起进行设置
-    read -p "Use 1.file_server or 2.reverse_proxy? (Default 1) : " mode
+    read -p "Use 1.file_server 2.reverse_proxy 3.respond 403? (Default 3) : " mode
     case "${mode}" in
     2)
         echo "Using reverse_proxy mode"
         read -p "Please Input Your Proxy URL(eg. http(s)://abc.de): " ppppp
-        sed -i -E "10,+3s|^#+||g" ./Caddyfile
-        sed -i -E "8,+1s|^|#|g" ./Caddyfile
-        sed -i -E "10s|reverse_proxy \* .*|reverse_proxy \* $ppppp {|g" ./Caddyfile
+        sed -i -E "11,+3s|^#+||g" ./Caddyfile
+        sed -i -E "9,+1s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "21s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "11s|reverse_proxy \* .*|reverse_proxy \* $ppppp {|g" ./Caddyfile
+        ;;
+    1)
+        echo "Using file_server mode"
+        sed -i -E "11,+3s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "21s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "9,+1s|^#+||g" ./Caddyfile
         ;;
     *)
-        echo "Using file_server mode"
-        sed -i -E "10,+3s|^|#|g" ./Caddyfile
-        sed -i -E "8,+1s|^#+||g" ./Caddyfile
+        sed -i -E "21s|^#+||g" ./Caddyfile
+        sed -i -E "11,+3s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "9,+1s|^(#+)?|#|g" ./Caddyfile
         ;;
     esac
     ChangeSettings "FQDN" "$FQDN"
@@ -111,7 +119,7 @@ ChangeUUID() {
         'y' | 'Y')
             sed -i -E "s|\w{8}(-\w{4}){3}-\w{12}\" //GRPC|$UUIDN1\" //GRPC|g" ./config.json
             sed -i -E "s|\"serviceName\": \".*\"|\"serviceName\": \"$UUIDN1\"|g" ./config.json
-            sed -i -E "5s|\/.+\/\*|\/$UUIDN1\/\*|g" ./Caddyfile
+            sed -i -E "s|path.+\/\w{8}(-\w{4}){3}-\w{12}\/\*|path  /$UUIDN1/*|g" ./Caddyfile
             ChangeSettings "UUID" "$UUIDN1"
             ;;
         *) ;;
@@ -120,16 +128,41 @@ ChangeUUID() {
     *)
         sed -i -E "s|\w{8}(-\w{4}){3}-\w{12}\" //GRPC|$UUIDN1\" //GRPC|g" ./config.json
         sed -i -E "s|\"serviceName\": \".*\"|\"serviceName\": \"$UUIDN1\"|g" ./config.json
-        sed -i -E "5s|\/.+\/\*|\/$UUIDN1\/\*|g" ./Caddyfile
+        sed -i -E "s|path.+\/\w{8}(-\w{4}){3}-\w{12}\/\*|path  /$UUIDN1/*|g" ./Caddyfile
         ChangeSettings "UUID" "$UUIDN1"
         ;;
     esac
 }
 
+SetCert() {
+#    if [ -e "./tls/cert.crt" ] && [ -e "./tls/key.key" ]; then
+#        echo "Path \"./tls\" exists, using external certs."
+#        sed -i -E "2s|^(#+)?|#|g" ./Caddyfile
+#        sed -i -E "3s|^#+||g" ./Caddyfile
+#    else
+        echo "Applying for certs from Let's Encrypt."
+        sed -i -E "3s|^(#+)?|#|g" ./Caddyfile
+        sed -i -E "2s|^#+||g" ./Caddyfile
+        FQDN=$(grep FQDN .settings | awk -F= '{print $2}')
+        docker-compose exec acme --issue --dns dns_cf -d $FQDN --server letsencrypt
+        docker-compose exec acme --install-cert -d $FQDN --key-file /tls/key.key --fullchain-file /tls/cert.crt
+#    fi
+
+}
+
+ShowShareLink() {
+    UUID=$(grep 'UUID' ./.settings | awk -F= '{print $2}')
+    FQDN=$(grep FQDN .settings | awk -F= '{print $2}')
+    sharelink="vless://$UUID@$FQDN:443?security=tls&serviceName=$UUID&type=grpc#$FQDN"
+    echo "$sharelink"
+}
+
 Install() {
     if [ -e ".settings" ] && [ -n "$(grep 'UUID' ./.settings | awk -F= '{print $2}')" ]; then
         UUID=$(grep 'UUID' ./.settings | awk -F= '{print $2}')
-        sed -i -E '/#'"$UUID"'$/d' /var/spool/cron/crontabs/"$(whoami)"
+        crontab -l > ./.cron
+        sed -i -E '/#'"$UUID"'$/d' ./.cron
+        crontab ./.cron
     fi
     Precheck 1
     ChangeCF
@@ -137,17 +170,69 @@ Install() {
     ChangeUUID
     docker-compose down --rmi all
     docker-compose up -d
-    FQDN=$(grep FQDN .settings | awk -F= '{print $2}')
-    docker-compose exec acme --issue --dns dns_cf -d $FQDN --server letsencrypt
-    docker-compose exec acme --install-cert -d $FQDN --key-file /tls/key.key --fullchain-file /tls/cert.crt
+    SetCert
     docker-compose restart
-    if [ -e "/usr/bin/docker-compose" ]; then
-        DockerComposePath="/usr/bin/docker-compose"
-        echo "0 0 1 * * cd $PWD && $DockerComposePath restart #$UUID" >>/var/spool/cron/crontabs/"$(whoami)"
-    elif [ -e "/usr/local/bin/docker-compose" ]; then
-        DockerComposePath="/usr/local/bin/docker-compose"
-        echo "0 0 1 * * cd $PWD && $DockerComposePath restart #$UUID" >>/var/spool/cron/crontabs/"$(whoami)"
+    if [ -e ".settings" ] && [ -n "$(grep 'UUID' ./.settings | awk -F= '{print $2}')" ]; then
+        UUID=$(grep 'UUID' ./.settings | awk -F= '{print $2}')
+        crontab -l > ./.cron
+        if [ -e "/usr/bin/docker-compose" ]; then
+            DockerComposePath="/usr/bin/docker-compose"
+            echo "0 0 1 * * cd $PWD && $DockerComposePath restart #$UUID" >>./.cron
+        elif [ -e "/usr/local/bin/docker-compose" ]; then
+            DockerComposePath="/usr/local/bin/docker-compose"
+            echo "0 0 1 * * cd $PWD && $DockerComposePath restart #$UUID" >>./.cron
+        fi
+        crontab ./.cron
     fi
+    ShowShareLink
+}
+
+Update() {
+    echo -e "1.xray\t2.caddy\t3.acme\t4.this script"
+    read -p ": " opt
+    case "${opt}" in
+    1)
+        imageID=$(docker-compose images | grep xray | awk '{print $4}')
+        if [ -z $imageID ]; then
+            echo "target image not found"
+            exit 0
+        fi
+        docker-compose rm -f -s xray
+        docker rmi $imageID
+        docker-compose up -d xray
+        ;;
+    2)
+        imageID=$(docker-compose images | grep caddy | awk '{print $4}')
+        if [ -z $imageID ]; then
+            echo "target image not found"
+            exit 0
+        fi
+        docker-compose rm -f -s caddy
+        docker rmi $imageID
+        docker-compose up -d caddy
+        ;;
+    3)
+        docker-compose exec acme --upgrade
+        ;;
+    4)
+        #curl -O "https://raw.githubusercontent.com/starP-W/caddy-grpc-docker/main/install.sh"
+        echo "还在考虑要不要"
+        ;;
+    *)
+        echo "神奇海螺"
+        ;;
+    esac
+
+}
+
+Uninstall() {
+    if [ -e ".settings" ] && [ -n "$(grep 'UUID' ./.settings | awk -F= '{print $2}')" ]; then
+        UUID=$(grep 'UUID' ./.settings | awk -F= '{print $2}')
+        crontab -l ./.cron
+        sed -i -E '/#'"$UUID"'$/d' ./.cron
+        crontab ./.cron
+    fi
+    docker-compose down --rmi all
 }
 
 echo -e "1.install\t2.update\t3.uninstall"
@@ -156,8 +241,11 @@ case "${opt}" in
 1)
     Install
     ;;
-2 | 3)
-    echo "还没写"
+2)
+    Update
+    ;;
+3)
+    Uninstall
     ;;
 *)
     echo "神奇海螺"
